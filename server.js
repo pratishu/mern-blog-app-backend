c775c82b425fa08855b40032a4eb94160490b218
pratishu <pratishu.kr@gmail.com>
2024-01-15 05:26:14 +0530

    cors

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+
 const port = process.env.PORT;
-
 const app = express();
-
-const person1 = {
-  name: "prat",
-  age: 23,
-  address: "somewhere on planet",
-};
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json(person1);
+  res.json({ name: "someone" });
 });
 
-app.listen(port || 3000); // 3000 is fallback port, when .env is not setup
+app.listen(port || 3000, () => {
+  console.log("app is listening to port 3002");
+}); // 3000 is fallback port, when .env is not setup
