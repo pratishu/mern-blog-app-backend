e016f58fde830633578b858457685f94c32cad2e
pratishu <pratishu.kr@gmail.com>
2024-01-15 05:04:56 +0530

    express setup with .env file

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-console.log("first init");
-const timeout = setTimeout(() => {
-  console.log(
-    "this log is after 3 second to check if nodemon is wroking correctly"
-  );
-}, 3000);
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT;
+
+const app = express();
+
+const person1 = {
+  name: "prat",
+  age: 23,
+  address: "somewhere on planet",
+};
+
+app.get("/", (req, res) => {
+  res.json(person1);
+});
+
+app.listen(port || 3000); // 3000 is fallback port, when .env is not setup
